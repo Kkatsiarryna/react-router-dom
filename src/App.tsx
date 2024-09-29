e1a3f53fe9ce3a184037f017f7c8a2837831b265
Kkatsiarryna <katepicastor@gmail.com>
2024-09-29 18:27:41 +0300

    react router dom 6.4

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React, {useState} from 'react';
-import {Navigate, NavLink, Route, Routes} from 'react-router-dom';
+import {Navigate, NavLink, Outlet, Route, Routes} from 'react-router-dom';
 import styles from "./components/Site.module.css";
-import {Adidas} from "./components/pages/Adidas";
-import {Puma} from "./components/pages/Puma";
-import {Abibas} from "./components/pages/Abibas";
-import {Error404} from "./components/pages/Error404";
 import {s} from "./components/pages/_styles"
 import {Model} from "./components/pages/Model";
-import {Prices} from "./components/pages/Prices";
+
 
 export const PATH = {
     ADIDAS: "/adidas",
@@ -31,20 +27,21 @@ function App() {
                     <s.NavWrapper><NavLink to={PATH.PRICES}>Цены для оптовиков</NavLink></s.NavWrapper>
                 </s.Nav>
                 <s.Content>
-                    <Routes>
-                        <Route path="/" element={<Navigate to={PATH.ADIDAS}/>}/>
-                        <Route path={PATH.ADIDAS} element={<Adidas/>}/>
-                        <Route path={PATH.PUMA} element={<Puma/>}/>
-                        <Route path={PATH.ABIBAS} element={<Abibas/>}/>
-                        <Route path={PATH.PRICES} element={<Prices/>}/>
+                    <Outlet />
+                    {/*<Routes>*/}
+                    {/*    <Route path="/" element={<Navigate to={PATH.ADIDAS}/>}/>*/}
+                    {/*    <Route path={PATH.ADIDAS} element={<Adidas/>}/>*/}
+                    {/*    <Route path={PATH.PUMA} element={<Puma/>}/>*/}
+                    {/*    <Route path={PATH.ABIBAS} element={<Abibas/>}/>*/}
+                    {/*    <Route path={PATH.PRICES} element={<Prices/>}/>*/}
 
-                        {/*<Route path={'adidas/:id'} element={<Model/>}/> */}
-                        <Route path={'/:model/:id'} element={<Model/>}/>
+                    {/*    /!*<Route path={'adidas/:id'} element={<Model/>}/> *!/*/}
+                    {/*    <Route path={'/:model/:id'} element={<Model/>}/>*/}
 
-                        {/*<Route path={PATH.ERROR} element={<Error404/>}/>*/}
-                        {/*<Route path="/*" element={<Navigate to={PATH.ERROR}/>}/>  */}
-                        <Route path="/*" element={<Error404/>}/>
-                    </Routes>
+                    {/*    /!*<Route path={PATH.ERROR} element={<Error404/>}/>*!/*/}
+                    {/*    /!*<Route path="/*" element={<Navigate to={PATH.ERROR}/>}/>  *!/*/}
+                    {/*    <Route path="/*" element={<Error404/>}/>*/}
+                    {/*</Routes>*/}
                 </s.Content>
             </s.Body>
             <s.Footer>abibas 2023</s.Footer>
